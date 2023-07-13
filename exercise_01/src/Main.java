3cc042579ad4a790444c821383b438c716f19613
MatheusLOliveira <matheuslimaoliveira@gmail.com>
2023-07-12 22:42:53 -0300

    Improving Exercise_01

--- exercise_01/src/Main.java
+++ exercise_01/src/Main.java
@@ -1,16 +1,19 @@
-/*Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
-C = 5 * ((F-32) / 9).*/
-
 import java.util.Scanner;
 
 public class Main {
+
+    /*
+    *   Exercício 1
+    *   Faça um Programa que peça a temperatura em graus Fahrenheit,
+    *   transforme e mostre a temperatura em graus Celsius.
+    *   C = 5 * ((F-32) / 9).
+    */
 
     public static double fahrenheitToCelsius(double fahrenheit) {
         return 5 * ((fahrenheit - 32) / 9);
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite a temperatura em graus Fahrenheit: ");
@@ -19,7 +22,7 @@ public class Main {
 
         double celsius = fahrenheitToCelsius(fahrenheit);
 
-        System.out.printf("A temperatura em graus Celsius é de: %.2f ºC", celsius);
+        System.out.printf("A temperatura %.2f F em graus Celsius é de: %.2f ºC",fahrenheit, celsius);
 
         sc.close();
     }
